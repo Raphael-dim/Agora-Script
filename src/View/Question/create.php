0021d10547d5db52b6d6ea2d4af4bb78bc6a233f
Krozac <theo.guenezan@gmail.com>
2022-10-19 22:32:55 +0200

    vue question

--- src/View/Question/create.php
+++ src/View/Question/create.php
@@ -1,13 +1,13 @@
 
 <?php
 
-if ((isset($_POST["Titre"]) && isset($_POST["nbSection"]))){
+if ((isset($_POST["Titre"]) && isset($_POST["nbSection"])) ){
     $Titre = htmlspecialchars($_POST["Titre"]);
     $nbSection = htmlspecialchars($_POST["nbSection"]);
 }
 else{
-    $Titre =" ";
-    $nbSection=0;
+    $Titre ="";
+    $nbSection="";
 }
 
 ?>
@@ -16,11 +16,11 @@ else{
             <legend>Mon formulaire :</legend>
             <p>
                 <label for="titre_id">Titre</label> :
-                <input type="text" placeholder="Ex : " value = <?php echo $Titre ?> name="Titre" id="titre_id" required/>
+                <input type="text" placeholder="Ex : "value = "<?php echo $Titre; ?>" name="Titre" id="titre_id" required/>
             </p>
             <p>
                 <label for="nbSections_id">Nombre de sections</label> :
-                <input type="text" placeholder="Ex : " value = <?php echo $nbSection ?>  name="nbSection" id="nbSections_id" required/>
+                <input type="text" placeholder="Ex : " value = "<?php echo$nbSection; ?>"  name="nbSection" id="nbSections_id" required/>
             </p>
         </fieldset>
         <input type="submit" value="Mettre à jour"/>
@@ -32,7 +32,13 @@ else{
         <fieldset>
 
             <?php
-            $nb = $nbSection;
+
+            if (!isset($nbSection)){
+                $nb = 0;
+            }
+            else{
+                $nb = $nbSection;
+            }
             for ($i = 1; $i <= $nb; $i++) {
                 echo "<p>
             <label for='nbSections_id'>Titre de la section n°" . $i . "</plabel> :
