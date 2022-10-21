8d1426efece275b3eec08ab5c7894c0008411e16
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2022-10-21 12:43:19 +0200

    classes repository

---
+++ src/View/Utilisateurs/created.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Utilisateur crée</title>
+</head>
+<body>
+<p>L'utilisateur a bien été créée !</p>
+<?php
+require __DIR__ . "/list.php";
+?>
+</body>
+</html>
+<?php
