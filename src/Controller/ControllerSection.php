5fc03c0e36b28d631d7f5ffd11aa9ce25ecb69f5
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2022-10-21 12:32:15 +0200

    classes repository save

--- src/Controller/ControllerSection.php
+++ src/Controller/ControllerSection.php
@@ -1,1 +1,14 @@
 <?php
+
+namespace App\Vote\Controller;
+
+class ControllerSection
+{
+
+
+    private static function afficheVue(string $cheminVue, array $parametres = []): void
+    {
+        extract($parametres); // Crée des variables à partir du tableau $parametres
+        require "../src/view/$cheminVue"; // Charge la vue
+    }
+}
