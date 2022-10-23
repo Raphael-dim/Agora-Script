4f9b2c71b8549d9ebb32da3b54b9a888dc7688d9
Krozac <theo.guenezan@gmail.com>
2022-10-23 02:16:32 +0200

    formulaire question

---
+++ src/View/Question/create/step-1.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+use App\Vote\Config\FormConfig as FormConfig;
+
+if (isset($_POST['Titre'])){
+    FormConfig::postSession();
+    $_SESSION['step'][1] = 1;
+    if (!isset($_SESSION['auteurs']) && !isset($_SESSION['votants'])){
+        $_SESSION['auteurs'] = array();
+        $_SESSION['votants'] = array();
+
+    }
+    FormConfig::redirect("index.php?controller=question&action=form&step=2");
+}
+
+?>
+
+<h1>Création d'un question</h1>
+
+
+<form method="post" >
+
+        <p>
+            <label for="titre_id">Titre</label> :
+            <input type="text" placeholder="Ex : " name="Titre" id="titre_id" value = "<?=FormConfig::TextField('Titre')?>" required/>
+        </p>
+        <p>
+            <label for="nbSections_select">Nombre de sections</label>
+            <select name="nbSections" id="nbSections_select">
+                <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<option value=" . $i .FormConfig::DropDown("nbSections",$i). " >" . $i . "</option>";
+
+                } ?>
+            </select>
+        </p>
+
+    <input type="submit" value="Suivant"/>
+</form>
+
+    <?php
+    if (isset($message)) {
+        echo "<p><img src=\"/web/images/attention.png\" class=\"attention\" > " . $message . "</p>";
+    } ?>
+
+
---
+++ src/View/Question/create/step-5.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+use App\Vote\Config\FormConfig as FormConfig;
+
+$_SESSION['type'] = 'votants';
+
+if(array_key_exists('user', $_POST)) {
+    adduser($_POST["user"]);
+}
+if(array_key_exists('delete', $_POST)) {
+    removeuser($_POST["delete"]);
+}
+if (isset($_POST['next'])){
+    FormConfig::postSession();
+    $_SESSION['step'][3] = 3;
+    FormConfig::redirect("index.php?controller=question&action=form&step=6");
+}else if (isset($_POST['previous'])){
+    FormConfig::postSession();
+    FormConfig::redirect("index.php?controller=question&action=form&step=4");
+}
+
+
+
+function adduser(String $id) : void
+{
+    if (!in_array($id,$_SESSION[$_SESSION['type']]))
+    {
+        $_SESSION[$_SESSION['type']][] = $id;
+    }
+}
+
+function removeuser(String $id) : void
+{
+
+    if (($key = array_search($id, $_SESSION[$_SESSION['type']])) !== false){
+        unset($_SESSION[$_SESSION['type']][$key]);
+    }
+}
+
+
+require_once "../src/View/Utilisateurs/select.php";
+?>
+<form method="post">
+    <input type="submit" name=next value="Suivant"/>
+    <input type="submit" name=previous value="Retour" formnovalidate/>
+</form>
