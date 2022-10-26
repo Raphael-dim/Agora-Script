091acc63927b82b35ba666757332b716b0acad7e
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2022-10-26 15:46:09 +0200

    select all

--- src/Model/Repository/SectionRepository.php
+++ src/Model/Repository/SectionRepository.php
@@ -14,6 +14,7 @@ class SectionRepository extends AbstractRepository
             $questionTableau["description"]
         );
         $section->setId($questionTableau["idsection"]);
+        return $section;
     }
 
     protected function getNomTable(): string
