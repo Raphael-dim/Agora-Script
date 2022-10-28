69c9fb433219740b3062a333d0793e395b20ad2e
Krozac <theo.guenezan@gmail.com>
2022-10-28 10:30:08 +0200

    commentaires

--- src/Model/Repository/AbstractRepository.php
+++ src/Model/Repository/AbstractRepository.php
@@ -8,7 +8,10 @@ use PDOException;
 
 abstract class AbstractRepository
 {
-
+    /*
+     * Sauvegarde l'objet dans la base de données
+     * en récupérant les colonnes et la table associés à la classe
+     */
     public function sauvegarder(AbstractDataObject $object): ?int
     {
         $sql = "INSERT INTO " . $this->getNomTable();
@@ -38,8 +41,12 @@ abstract class AbstractRepository
             $max = $retour[0];
             return $max;
         }
+        return null;
     }
 
+    /*
+     * Supprime la ligne de la base de données graçe à la clef primaire
+     */
     public function delete(string $valeurClePrimaire)
     {
         $sql = "DELETE FROM " . $this->getNomTable() . " WHERE " . $this->getNomClePrimaire() . " =:clePrimaireTag";
@@ -54,6 +61,9 @@ abstract class AbstractRepository
         }
     }
 
+    /*
+     * met à jour l'objet dans la base de données
+     */
     public function update(AbstractDataObject $object)
     {
         $sql = "UPDATE " . $this->getNomTable() . "
@@ -72,6 +82,9 @@ abstract class AbstractRepository
         }
     }
 
+    /*
+     * Selectionne toute les lignes de la table associé à la classe
+     */
     public function selectAll(): array
     {
         $ADonnees = array();
@@ -84,6 +97,10 @@ abstract class AbstractRepository
         return $ADonnees;
     }
 
+
+    /*
+     * Selectionne les lignes par rapport à un mot clef
+     */
     public function selectKeyword($motclef, $row)
     {
         $ADonnees = array();
@@ -104,6 +121,9 @@ abstract class AbstractRepository
         return $ADonnees;
     }
 
+    /*
+     * Selectionne une ligne par rapport à la clef primaire
+     */
     public function select($clef)
     {
         $sql = 'SELECT * from ' . $this->getNomTable() . ' WHERE ' . $this->getNomClePrimaire() . '=:clef';
@@ -126,11 +146,23 @@ abstract class AbstractRepository
         return $this->construire($data);
     }
 
+    /*
+     * Retourne le nom de la table associé à la classe
+     */
     protected abstract function getNomTable(): string;
 
+    /*
+     * Transforme un tableau de donnée en Objet
+     */
     protected abstract function construire(array $objetFormatTableau);
 
+    /*
+     * Retourne le nom de la clef primaire de la table
+     */
     protected abstract function getNomClePrimaire(): string;
 
+    /*
+     * Retourne les noms de colonnes de la table
+     */
     protected abstract function getNomsColonnes(): array;
 }
