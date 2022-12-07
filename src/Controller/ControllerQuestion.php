93cf63221688b400f520b25bb367292d1fa5d4d3
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2022-12-07 14:58:46 +0100

    connexion et messages flashs

--- src/Controller/ControllerQuestion.php
+++ src/Controller/ControllerQuestion.php
@@ -218,7 +218,6 @@ class ControllerQuestion
         FormConfig::startSession();
     }
 
-<<<<<<<<< Temporary merge branch 1
     public static function update(): void
     {
         $question = (new QuestionRepository())->select($_GET['idQuestion']);
@@ -231,15 +230,13 @@ class ControllerQuestion
             ControllerAccueil::erreur();
         } else {
             FormConfig::setArr('SessionQuestion');
-            FormConfig::initialiserSessions($question);
+            FormConfig::startSession();
             self::afficheVue('view.php', ["pagetitle" => "Modifier une question",
                 "cheminVueBody" => "question/create/step-1.php",
                 "idQuestion" => $_GET['idQuestion']]);
         }
     }
 
-=========
->>>>>>>>> Temporary merge branch 2
     public static function updated(): void
     {
         $user = Session::getInstance()->lire('user');
@@ -337,26 +334,6 @@ class ControllerQuestion
         FormConfig::startSession();
     }
 
-    public static function update(): void
-    {
-        $question = (new QuestionRepository())->select($_GET['idQuestion']);
-        $user = Session::getInstance()->lire('user');
-        if (is_null($user) || $user['id'] != $question->getOrganisateur()->getIdentifiant()) {
-            MessageFlash::ajouter("warning", "Vous ne pouvez pas modifier une question dont vous n'êtes par l'organisateur.");
-            Controller::redirect("index.php?action=readAll&controller=question");
-        }
-        if (!isset($_SESSION['user']) || $_SESSION['user']['id'] != $question->getOrganisateur()->getIdentifiant()) {
-            ControllerAccueil::erreur();
-        } else {
-            FormConfig::setArr('SessionQuestion');
-            FormConfig::startSession();
-            self::afficheVue('view.php', ["pagetitle" => "Modifier une question",
-                "cheminVueBody" => "question/create/step-1.php",
-                "idQuestion" => $_GET['idQuestion']]);
-        }
-    }
-
-
     public static function delete(): void
     {
         $user = Session::getInstance()->lire('user');
@@ -380,7 +357,8 @@ class ControllerQuestion
             (new CalendrierRepository())->delete($calendrier->getId());
             $questions = (new QuestionRepository())->selectAll(); //appel au modèle pour gerer la BD
             MessageFlash::ajouter('success', 'La question a bien été supprimée');
-            Controller::redirect("index.php?controller=question&action=readAll");        }
+            Controller::redirect("index.php?controller=question&action=readAll");
+        }
     }
 
     public static function readKeyword(): void
