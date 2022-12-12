f807be71c85a992a1df961f6ea28e0a139bebe50
naelf <nael.fradet@gmail>
2022-12-12 11:12:25 +0100

    commit debut page resultat

--- src/View/Question/resultat.php
+++ src/View/Question/resultat.php
@@ -11,32 +11,51 @@
 
     echo'<div>
             <div class="premier">
-                 <div class="divDePremier">
-                        <p class = "listes">
-                            <a href= index.php?action=read&controller=proposition&idProposition=' .
-                            $idPropositionURL . '>' . 1 . ' : ' . $titreHTML . '  </a>
-                            Nombre de votes :' . $propositions[0]->getNbVotes().'
-                        </p>
-                 </div>
-             </div>
+                <div class=proposition>
+                    <a href= index.php?action=read&controller=proposition&idProposition=' .
+                    $idPropositionURL . '> <h2>1. ' . $titreHTML . '</h2>   </a>
+                    <br>
+                    <h3>Nombre de votes : ' . $propositions[0]->getNbVotes() . '</h3>
+                    <a href="" id = "auteur">par ' . $propositions[0]->getResponsable()->getIdentifiant() . ' </a >
+                    <img class="imgPremier" src="images/premier.png">
+                </div>
+            </div>
              <div class="deuxTrois">
                  <div class="deuxieme">  
-                    <p class = "listes">
+                    <div class=proposition>
                         <a href= index.php?action=read&controller=proposition&idProposition=' .
-                        rawurlencode($propositions[1]->getId()) . '>' . 2 . ' : ' . htmlspecialchars($propositions[1]->getTitre()) . '  </a>
-                        Nombre de votes :' . $propositions[1]->getNbVotes().'
-                    </p>   
+                        rawurlencode($propositions[1]->getId()) . '> <h2>2. ' . htmlspecialchars($propositions[1]->getTitre()) . '</h2>   </a>
+                        <br>
+                        <h3>Nombre de votes : ' . $propositions[1]->getNbVotes() . '</h3>
+                        <a href="" id = "auteur">par ' . $propositions[1]->getResponsable()->getIdentifiant() . ' </a >
+                        
+                </div>  
                  </div>
                  
                  <div class="troisieme">  
-                    <p class = "listes">
+                    <div class=proposition>
                         <a href= index.php?action=read&controller=proposition&idProposition=' .
-                        rawurlencode($propositions[2]->getId()) . '>' . 3 . ' : ' . htmlspecialchars($propositions[2]->getTitre()) . '  </a>
-                        Nombre de votes :' . $propositions[2]->getNbVotes().'
-                    </p>    
+                        rawurlencode($propositions[2]->getId()) . '> <h2>1. ' . htmlspecialchars($propositions[2]->getTitre()) . '</h2>   </a>
+                        <br>
+                        <h3>Nombre de votes : ' . $propositions[2]->getNbVotes() . '</h3>
+                        <a href="" id = "auteur">par ' . $propositions[2]->getResponsable()->getIdentifiant() . ' </a >
+                </div>  
                  </div>
              </div>
          </div>';
 
+
+    foreach ($propositions as $proposition) {
+        /*$idPropositionURL = rawurlencode($proposition->getId());
+        $titreHTML = htmlspecialchars($proposition->getTitre());
+        echo '<div class=proposition>';
+        echo ' <a href= index.php?action=read&controller=proposition&idProposition=' .
+            $idPropositionURL . '> <h2>' . $titreHTML . '</h2>   </a>';
+        echo '<br > ';
+        echo '<h3>Nombre de votes : ' . $proposition->getNbVotes() . '</h3>';
+        $i++;
+        echo '<a href="" id = "auteur">par ' . $proposition->getResponsable()->getIdentifiant() . ' </a >';
+        echo '</div>';*/
+    }
     ?>
 </ul>
