e197759330fdf272b1fa032b705391f40d69454c
naelf <nael.fradet@gmail>
2022-12-12 20:23:35 +0100

    commit page resultat + confettis

--- src/View/Question/resultat.php
+++ src/View/Question/resultat.php
@@ -37,7 +37,7 @@
                  <div class="troisieme">  
                     <div class=proposition>
                         <a href= index.php?action=read&controller=proposition&idProposition=' .
-                        rawurlencode($propositions[2]->getId()) . '> <h2>1. ' . htmlspecialchars($propositions[2]->getTitre()) . '</h2>   </a>
+                        rawurlencode($propositions[2]->getId()) . '> <h2>3. ' . htmlspecialchars($propositions[2]->getTitre()) . '</h2>   </a>
                         <br>
                         <h3>Nombre de votes : ' . $propositions[2]->getNbVotes() . '</h3>
                         <a href="" id = "auteur">par ' . $propositions[2]->getResponsable()->getIdentifiant() . ' </a >
@@ -47,18 +47,32 @@
              </div>
          </div>';
 
-
+    echo '<div class="propositions">';
     for ($i=3;$i<count($propositions);$i++) {
         $idPropositionURL = rawurlencode($propositions[$i]->getId());
         $titreHTML = htmlspecialchars($propositions[$i]->getTitre());
         echo '<div class=proposition>';
         echo ' <a href= index.php?action=read&controller=proposition&idProposition=' .
-            $idPropositionURL . '> <h2>' . $titreHTML . '</h2>   </a>';
+            $idPropositionURL . '> <h2>' . $i+1 .'. '. $titreHTML . '</h2>   </a>';
         echo '<br > ';
         echo '<h3>Nombre de votes : ' . $propositions[$i]->getNbVotes() . '</h3>';
         echo '<a href="" id = "auteur">par ' . $propositions[$i]->getResponsable()->getIdentifiant() . ' </a >';
         echo '</div>';
         $i++;
     }
+    echo '</div>';
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+            confetti({
+                spread: 360,
+                particleCount: 450,
+                origin:{
+                    x: 0.5,
+                    y: 0.2
+                }
+            })
+    </script>
+    <script type="text/javascript" src="js/jquery-3.4.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
 </ul>
