c1fbec77666ea64713deb550ce88e10e2c9a1dda
naelf <nael.fradet@gmail>
2023-01-08 21:29:41 +0100

    resultat et proposition

--- src/View/Proposition/detail.php
+++ src/View/Proposition/detail.php
@@ -12,20 +12,20 @@ use App\Vote\Model\DataObject\Question;
 <h1><strong class="custom_strong color-grey">Titre question : </strong></h1>
 <h2><?= htmlspecialchars($question->getTitre()) ?></h2>
 <h1><strong class="custom_strong color-grey">Description question : </strong></h1>
-<h2><?= htmlspecialchars($question->getDescription()) ?></h2>
+<h2 id="description"><?= htmlspecialchars($question->getDescription()) ?></h2>
 
 <?php
 
 $i = 1;
 ?>
-<h1><strong class="color-blue">Titre de la proposition : </strong></h1>
+<h1><strong class="custom_strong color-blue">Titre de la proposition : </strong></h1>
 <?php
 echo '<h1>' . htmlspecialchars($proposition->getTitre()) . '</h1>';
 echo '
 <div id="participants" class="detail_question">';
 echo '
     <div>';
-echo '<h1><strong class=" custom_strongcolor-yellow">Auteur</strong></h1>';
+echo '<h1><strong class="custom_strong color-yellow">Auteur</strong></h1>';
 
 if (!is_null($proposition->getIdResponsable())) {
     echo "<p>" . htmlspecialchars($proposition->getIdResponsable()) . "</p>";
@@ -36,7 +36,7 @@ echo '
     ';
 echo '
     <div id="votants">';
-echo '<h1><strong class=" custom_strongcolor-yellow">Co-Auteurs</strong></h1>';
+echo '<h1><strong class=" custom_strong color-yellow">Co-Auteurs</strong></h1>';
 if (!is_null($coAuts)) {
     if (is_array($coAuts)) {
         foreach ($coAuts as $coAut) {
@@ -57,13 +57,13 @@ echo '
 $propSection = (new PropositionSectionRepository())->selectWhere($proposition->getId(), '*', 'idproposition', 'Proposition_section');
 foreach ($sections as $section) {
     echo '
-<div>';
-    echo '<h2>Section n°' . $i . '</h2>';
-    echo '<p>Titre : ' . htmlspecialchars($section->getTitre()) . ' </p> ';
-    echo '<p>Description : ' . htmlspecialchars($section->getDescription()) . ' </p> ';
+<div id="detail_section" class="detail_question" >';
+    echo '<h1><strong class=" custom_strong color-yellow">Section n°' . $i . '</strong></h1></h1>';
+    echo '<h2><strong >Titre Section : </strong></h2><p>' . htmlspecialchars($section->getTitre()) . ' </p> ';
+    echo '<h2><strong >Description Section : </strong></h2><p>' . htmlspecialchars($section->getDescription()) . ' </p> ';
     echo '
     <p>
-        <span> Contenu</span> :
+        <h2><strong >Contenu : </strong></h2><p>
         ' . htmlspecialchars($propSection[$i - 1]->getContenu()) . '
     </p></div>';
 
