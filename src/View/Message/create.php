d2917c1516e3f0c91794b2a575b799357937f453
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2023-01-12 16:33:05 +0100

    fix

--- src/View/Message/create.php
+++ src/View/Message/create.php
@@ -3,23 +3,24 @@
     <fieldset>
         <h2 style="color: #022234">Envoyer un message :</h2>
         <p>
-            <label for="motclef"></label><input style="max-width: 200px" placeholder="Rechercher un utilisateur"
+            <label for="motclef"></label><input style="max-width: 300px" placeholder="Rechercher un utilisateur"
                                                 type="text"
                                                 name="keyword" id="motclef"
                                                 required>
             <input style="max-height: 35px" type="image" alt="Submit" src="../web/images/search.png" class="search">
         </p>
-        <?php
-        if (isset($utilisateurs)) {
-            foreach ($utilisateurs as $utilisateur) {
-                echo '<div class="contact">
+    </fieldset>
+    <input id="suivant" type="submit" value="Suivant" class="nav">
+</form>
+<?php
+if (isset($utilisateurs)) {
+    foreach ($utilisateurs as $utilisateur) {
+        echo '<div class="contact">
             <img style="max-height: 40px" src="images/profil.png" alt="profil">
             <a href="index.php?action=read&controller=utilisateur&idUtilisateur=' . $utilisateur->getIdentifiant() . '">' . htmlspecialchars($utilisateur->getPrenom()) . ' ' . htmlspecialchars($utilisateur->getNom()) . '</a>
-            <a style="min-width: 100%" href="index.php?controller=message&action=read&idContact=' . rawurlencode($utilisateur->getIdentifiant()) . '#dernierMessage">Envoyer un message</a></div>';
-            }
-        }
-        ?>
-        <input id="suivant" type="submit" value="Suivant" class="nav">
+            <a style="min-width: 100%" href="index.php?controller=message&action=read&idContact=' . rawurlencode($utilisateur->getIdentifiant()) . '#dernierMessage">Envoyer un message</a>
+            </div>';
+    }
+}
+?>
 
-    </fieldset>
-</form>
